1b6712e53f646a2e0a58dd24c30dd855fbc67f41
yashraj <yashrajjghosalkar@gmail.com>
2024-03-08 02:01:00 +0530

    Navbar creation

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+{/* <div className=" absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+  POPUP
+</div> */} {/*  creating 3D Objects below ...*/}
+
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+    </section>
+  )
+}
+
+export default Home
