7677a8ad0431ac2986f8a2dace748f9cd655142e
yashraj <yashrajjghosalkar@gmail.com>
2024-03-08 22:39:24 +0530

    Homepage setup
    setup Loader for 3d island, used react-three-drei for more optimization

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 {/* <div className=" absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
 </div> */} {/*  creating 3D Objects below ...*/}
@@ -7,7 +8,19 @@ import React from 'react'
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-    </section>
+      < Canvas 
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+      <Suspense fallback={<Loader />}>
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+      </Suspense>
+      </Canvas>
+    </section> 
   )
 }
 
