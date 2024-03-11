0f0af8b818885caef305bed2d2ec59f591ee9126
yashraj <yashrajjghosalkar@gmail.com>
2024-03-11 21:07:14 +0530

    updated

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, Suspense } from 'react'
+import { useState, Suspense, useEffect, useRef } from 'react'
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 
@@ -8,9 +8,28 @@ import Bird from '../models/Bird';
 import Plane from '../models/Plane';
 import HomeInfo from '../components/HomeInfo';
 
+import sakura from '../assets/sakura.mp3';
+import { soundoff, soundon } from '../assets/icons';
+
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura)); //change the audio from here
+  audioRef.current.volume = 0.2; //set the volume from here
+  audioRef.current.loop = true; // set tthe true or false do we want to loop audio
   const [isRotating, setIsRotating] = useState(false);  
   const [currentStage, setCurrentStage] = useState(1);
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+  useEffect( () => {
+     if(isPlayingMusic) {
+      audioRef.current.play();
+     }
+
+     return () => {
+      audioRef.current.pause();
+     }
+  }, [isPlayingMusic])
+
+
   const adjustIslandForScreenSize = () => {
     let screenScale =null;
     let screenPosition = [0, -6.5, -43];
@@ -75,6 +94,14 @@ const Home = () => {
         />
       </Suspense>
       </Canvas>
+      <div className="absolute bottom-2 left-2">
+        <img
+          src={! isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className="w-10 h-10 cursor-pointer object-contain"
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section> 
   )
 }
